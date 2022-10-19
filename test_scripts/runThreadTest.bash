c2b453e1b67a5291dfcefdcd65a0f27256e5b655
Anushka Hebbar <r.name>
2022-10-19 12:31:57 +0530

    add results

--- test_scripts/runThreadTest.bash
+++ test_scripts/runThreadTest.bash
@@ -7,9 +7,5 @@
 for i in {17..40..1}
 do
   echo "Number of threads: $i"
-  for j in 10000 50000 100000 500000 1000000
-  do
-    echo -e "\tMax prime: $j"
-    sysbench cpu --cpu-max-prime="$j" --time=30 --threads="$i" run >> "sysbenchOutputs/results_${i}_${j}.txt"
-  done
+  sysbench cpu --cpu-max-prime=1000000 --time=30 --threads=5 run >> "../test_outputs/instances_test/results_${i}_${j}.txt"
 done
